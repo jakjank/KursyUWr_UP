218c971797259e1aef76daaf50a62582a63b7bc2
Jakub Jankowski <kuba.jankowski345@gmail.com>
2025-03-16 19:30:04 +0100

    added .Net 3rd list

---
+++ .NET/Lista2/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,51 @@
+﻿class Grid
+{
+    int[,] grid;
+    public Grid(int no_rows, int no_cols)
+    {
+        grid = new int[no_rows, no_cols];
+    }
+
+    public int this[int row, int col]
+    {
+        get
+        {
+            return grid[row, col];
+        }
+        set
+        {
+            grid[row, col] = value;
+        }
+    }
+
+    public int[] this[int row]
+    {
+        get
+        {
+            int length = grid.GetLength(1);
+            int[] rowVals = new int[length];
+            for(int i = 0; i < length; i++)
+            {
+                rowVals[i] = grid[row, i];
+            }
+            return rowVals;
+        }
+    }
+}
+
+class Program
+{
+    public static void Main()
+    {
+        Grid grid = new(3, 4);
+        grid[0, 0] = 33;
+        grid[0, 3] = 44;
+        grid[2, 2] = 11;
+        Console.WriteLine(grid[2, 2]);
+        int[] vals = grid[0];
+        foreach(int val in vals)
+        {
+            Console.Write($"{val} ");
+        }
+    }
+}
---
+++ .NET/Lista2/Ex3/Ex3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Xml.Serialization;
+
+class Program
+{
+    class Example
+    {
+        private string? someString;
+        
+        public string SomeString
+        {
+            get { return someString; }
+            set { someString = value; }
+        }
+
+        public int? SomeInt { set; get; }
+    }
+
+    public static void Main()
+    {
+        Example example = new Example();
+        example.SomeString = "qwerty";
+        Console.WriteLine(example.SomeString);
+
+        example.SomeInt = 32;
+        Console.WriteLine(example.SomeInt);
+    }
+}
