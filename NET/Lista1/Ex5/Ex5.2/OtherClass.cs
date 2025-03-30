3eb0e23f03c2770d0fbbc65c46ba6fe6e13cd20b
Jakub Jankowski <kuba.jankowski345@gmail.com>
2025-03-30 23:03:35 +0200

    readded NET/List1

---
+++ NET/Lista1/Ex5/Ex5.2/OtherClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class OtherClass
+{
+    public static void checkInternal()
+    {
+        BaseClass obj = new BaseClass();
+        obj.ImInternal();
+        obj.ImPublic();
+    }
+}
+
