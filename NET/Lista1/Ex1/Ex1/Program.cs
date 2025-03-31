5039b7ca512259820d084289bb9df98fad36263a
Jakub <kuba.jankowski345@gmial.com>
2025-03-31 11:54:36 +0200

    NET list1 and list2

---
+++ NET/Lista1/Ex1/Ex1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        List<int> res = new List<int>();
+        for(int i=1; i<100000; i++)
+        {
+            if(dividesByDigitsSum(i) && dividesByDigits(i))
+            {
+                res.Add(i);
+            }
+        }
+
+        foreach(int el in res)
+        {
+            Console.Write(el + " ");
+        }
+    }
+    static bool dividesByDigitsSum(int a)
+    {
+        int tmp = a;
+        int digitSum = 0;
+        while(a > 0)
+        {
+            digitSum += a % 10;
+            a /= 10;
+        }
+        return (tmp % digitSum) == 0;
+    }
+
+    static bool dividesByDigits(int a)
+    {
+        int tmp = a;
+        int digit;
+        while (a > 0)
+        {
+            digit = a % 10;
+            if(digit == 0) { return false; }
+            if (tmp % digit != 0) { return false; }
+            a /= 10;
+        }
+        return true;
+    }
+}
---
+++ NET/Lista1/Ex6/Ex6/Program.cs
@@ -0,0 +1,71 @@
+﻿
+class Person
+{
+    string name;
+    int age;
+
+    public Person()
+    {
+        name = "unknowm";
+        age = -1;
+    }
+
+    public Person(string name, int age)
+    {
+        this.name = name;
+        this.age = age;
+    }
+
+    public virtual void describe()
+    {
+        Console.WriteLine($"{name},{age}");
+    }
+
+    public void describe(bool showAge)
+    {
+        if(showAge) this.describe();
+        else Console.WriteLine($"{name}");
+    }
+
+    //Wont work
+    //public string describe()
+    //{
+    //    return ($"({name},{age})";
+    //}
+
+    public string describe(string message)
+    {
+        return $"{name},{age},{message}";
+    }
+}
+
+class Actor : Person
+{
+    int noMovies;
+
+    public Actor(string name, int age, int noMovies) : base(name,age)
+    {
+        this.noMovies = noMovies;
+    }
+
+    public override void describe()
+    {
+        Console.Write($"{noMovies}, ");
+        base.describe();
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Person p1 = new Person();
+        Person tom = new Person("Tom", 33);
+        Actor anthony = new Actor("Anthony", 87, 123);
+
+        p1.describe();
+        tom.describe(false);
+        Console.WriteLine(tom.describe("Hi Im Tom"));
+        anthony.describe();
+    }
+}
