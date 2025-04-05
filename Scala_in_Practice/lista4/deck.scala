c037179e499679a7d60fb82f95682b7849ac87c1
Jakub <kuba.jankowski345@gmial.com>
2025-04-05 12:57:39 +0200

    redesigned list4 cards

--- Scala_in_Practice/lista4/deck.scala
+++ Scala_in_Practice/lista4/deck.scala
@@ -1,14 +1,21 @@
-import cards.{Card, colors, values}
+import cards.*
 import scala.util.Random.shuffle
+import scala.io.Source
 
 package deck{
     case class Deck(cards: List[Card]) {
         //creates new deck without first card
-        def pull() = new Deck(cards.tail)
+        def pull() : Deck = 
+            cards match {
+                case Nil => throw new IllegalStateException("Cannot draw card from empty deck!")
+                case x::xs => new Deck(xs)
+            }
         //creates new deck with given card pushed on top
+
         def push(c: Card) : Deck = new Deck(c :: cards)
         //creates new deck with new card(color, value) pushed on top
-        def push(color: String, value: Int) : Deck = push(new Card(color,value))
+
+        def push(color: Color, value: Rank) : Deck = push(new Card(color,value))
         
         // checks if deck is a standard deck
         val isStandard: Boolean = 
@@ -17,49 +24,47 @@ package deck{
                 if duplicatesOfCard(card) == 1
             } yield 1).sum == 52
         //amount of duplicates of the given card in the deck
+
         def duplicatesOfCard(card: Card): Int = 
             //cards.count(_ == card)
             (for{
                 deck_card <- cards
                 if deck_card == card
             } yield 1).sum
+
         //amount of cards in the deck for the given color
-        def amountOfColor(color: String): Int =
+        def amountOfColor(color: Color): Int =
             cards.count(_.color == color)
+
         //amount of cards in the deck for given numerical card (2, 3, 4, 5, 6, 7, 8, 9, 10)
-        def amountOfNumerical(numerical: Int): Int =
-            require(1 < numerical && 11 > numerical, "for Jack-Ace use amountOfFace")
-            cards.count(_.value == numerical)
+        def amountOfNumerical(numerical: Numerical): Int =
+            cards.count(_.rank == Numerical)
+
         //amount of all numerical cards in the deck (2, 3, 4, 5, 6, 7, 8, 9, 10)
         val amountWithNumerical: Int = 
-            (for{
-                card <- cards
-                if card.value < 11
-            } yield 1).sum
+            cards.count{
+                card => {
+                    card.rank match {
+                        case x : Numerical => true
+                        case _ => false
+                    }
+                }
+            }
+        
         //amount of cards in the deck for the given face (Jack, Queen & King)
-        def amountOfFace(face: String) : Int =  
-            require(face == "Jack" || face == "Queen" || face == "King" || face == "Ace",
-                    "return amount of Jacks - Aces in the deck")
-            if( face == "Jack" ){
-                return cards.count(_.value == 11)
-            }
-            if( face == "Queen" ){
-                return cards.count(_.value == 12)
-            }
-            if( face == "King" ){
-                return cards.count(_.value == 13)
-            }
-            return cards.count(_.value == 14)
+        def amountOfFace(face: Face) : Int =
+            cards.count(_.rank == face)
+            
         //amount of all cards in the deck with faces (Jack, Queen & King)
         val amountWithFace: Int = 
-            (for{
-                card <- cards
-                if card.value >= 11
-            } yield 1).sum
-        // override def toString : String =
-        //     (for{
-        //         card <- cards
-        //     } yield card.toString).toString
+            cards.count{
+                card => {
+                    card.rank match {
+                        case x : Face => true
+                        case _ => false
+                    }
+                }
+            }
     }
 
     object Deck{
@@ -70,9 +75,9 @@ package deck{
             new Deck(
                 shuffle(
                     (for {
-                    color <- colors
-                    value <- values
-                } yield new Card(color,value)))
+                        color <- List(Clubs, Diamonds, Hearts, Spades)
+                        rank <- (2 to 10).map(Numerical(_)) ++ List(Ace, Jack, Queen, King)
+                    } yield new Card(color,rank)))
             )
     }
 }
