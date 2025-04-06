da23c2cc51b54908cf764efc598d0c3280e62a80
Jakub Jankowski <kuba.jankowski345@gmail.com>
2025-04-06 16:03:53 +0200

    modified list 4

--- NET/Lista4/Ex8/Ex8/Program.cs
+++ NET/Lista4/Ex8/Ex8/Program.cs
@@ -5,7 +5,7 @@
         Func<Func<Func<int, int>, Func<int, int>>, Func<int, int>> Y = null;
         Y = f => x => f(Y(f))(x);
 
-        List<int> list = new List<int>() { 1, 2, 3, 4, 5, 6 };
+        List<int> list = [1, 2, 3, 4, 5, 6];
 
         foreach (var item in list.Select( Y(f => i => (i <= 2) ? 1 : f(i - 1) + f(i - 2))) )
         {
