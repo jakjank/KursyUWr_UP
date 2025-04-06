d04eeb84cda34d876b0f4c9dce72bf4f06ee2185
Jakub Jankowski <kuba.jankowski345@gmail.com>
2025-04-06 16:04:20 +0200

    added list 5

---
+++ NET/Lista5/Ex3/Ex3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+public static class IntAwaiterExtensions
+{
+    public static TaskAwaiter GetAwaiter(this int milliseconds)
+    {
+        return Task.Delay(milliseconds).GetAwaiter();
+    }
+}
+
+class Program
+{
+    public static async Task Main()
+    {
+        Console.WriteLine("1");
+        await 2000;
+        Console.WriteLine("2");
+    }
+}
