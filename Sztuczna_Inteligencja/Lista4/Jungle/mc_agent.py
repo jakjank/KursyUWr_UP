df884b7f3f3c1f13b83f51d642c6982e93d8b5e5
Jakub <kuba.jankowski345@gmail.com>
2025-05-22 10:14:53 +0200

    upgraded Jungle game logic

--- Sztuczna_Inteligencja/Lista4/Jungle/mc_agent.py
+++ Sztuczna_Inteligencja/Lista4/Jungle/mc_agent.py
@@ -3,47 +3,55 @@ import copy
 from random_game import *
 
 NO_RANDOM_GAMES = 1
-NO_MOVES_PER_CHOICE = 20_000
+NO_MOVES_PER_CHOICE = 2_000
 
 class mc_agent:
     
     def __init__(self, me):
         self.me = me
         self.simulated_moves = 0
+        self.counter= 50
 
     def get_move(self, board:Board):
         print(self.simulated_moves)
         moves = board.get_possible_moves(self.me)
         if len(moves) == 0:
             return (-1,-1,-1,-1)
+        # if self.counter > 0:
+        #     self.counter -= 1
+        #     return random.choice(moves)
         
         best_move = moves[0]
-        best_stats = 0
-        stats = 0
+        best_win_simulated_ratio = 0
 
         for move in moves:
+            # print(move)
             max_simulated_moves= NO_MOVES_PER_CHOICE / len(moves)
             my_board = copy.deepcopy(board)
             old_pos = (move[0],move[1])
             new_pos = (move[2],move[3])
             my_board.move(old_pos, new_pos, self.me)
-            stats = 0
-            while max_simulated_moves > 0:
+            played_games = 0
+            won_games = 0
+            while max_simulated_moves > 0 and played_games < 20:
+
                 if self.me == BIG:
                     not_me = SMALL
                 else:
                     not_me = BIG
-                my_board2 = copy.deepcopy(board)
+
+                my_board2 = copy.deepcopy(my_board)
                 who_won, NO_moves = random_game(my_board2, not_me)
+                played_games += 1
                 self.simulated_moves += NO_moves
                 max_simulated_moves -= NO_moves
                 if who_won == self.me:
-                    stats += 1
-                else:
-                    stats -= 1
+                    won_games += 1
+                
             #print(stats)
-            if stats > best_stats:
+            if won_games/played_games > best_win_simulated_ratio:
                 best_move = move
-                best_stats = stats
+                best_win_simulated_ratio = won_games/played_games
+
         return best_move
         
