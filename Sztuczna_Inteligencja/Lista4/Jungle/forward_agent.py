e392e224139e88e7bf640fdda42298841ee7b665
Jakub <kuba.jankowski345@gmail.com>
2025-05-22 16:54:12 +0200

    forward_agent made able to play BIG

--- Sztuczna_Inteligencja/Lista4/Jungle/forward_agent.py
+++ Sztuczna_Inteligencja/Lista4/Jungle/forward_agent.py
@@ -16,6 +16,16 @@ class forward_agent:
         #check if can go up
         position = board.get_position(animal, self.me)
         return (position[0], position[1], position[0]-1, position[1])
+    
+    def go_left(self, animal, board:Board):
+        #check if can go right
+        position = board.get_position(animal, self.me)
+        return (position[0], position[1], position[0], position[1]-1)
+    
+    def go_down(self, animal, board:Board):
+        #check if can go up
+        position = board.get_position(animal, self.me)
+        return (position[0], position[1], position[0]+1, position[1])
 
 
     def get_move(self, board:Board):
@@ -24,15 +34,23 @@ class forward_agent:
 
         if self.counter == 0:
             self.counter = 1
-            return (6,2,7,2)
+            if self.me == SMALL:
+                return (6,2,7,2)
+            return (2,4,1,4)
 
         if(board.players_board(self.me)[7] == (-1,-1)): # no elephant
             move = random.choice(board.get_possible_moves(self.me))
         else:
             if self.counter < 4:
-                move = self.go_right(7, board)
+                if self.me == SMALL:
+                    move = self.go_right(7, board)
+                else:
+                    move = self.go_left(7, board)
             else:
-                move = self.go_up(7,board)
+                if self.me == SMALL:
+                    move = self.go_up(7,board)
+                else:
+                    move = self.go_down(7,board)
 
         self.counter += 1
         return move
